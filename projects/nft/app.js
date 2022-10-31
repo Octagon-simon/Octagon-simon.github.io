0015fa1433118efd0483188b4226dab1440d2bb0
Simon Ugorji <ugorji757@gmail.com>
2022-10-31 10:13:02 +0100

    updated nft project

--- projects/nft/app.js
+++ projects/nft/app.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:3000/projects/nft";
+const url = "https://octagon-simon.github.io/projects/nft";
 //use $ as global query selector
 const $ = (elem) => document.querySelector(elem);
 //use $$ as global query selectorAll
